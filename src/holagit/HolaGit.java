9616f785aa80da174061c300525fa174400971a2
Mao Rivera Sanchez Armas <piratlax@gmail.com>
2016-12-22 16:01:35 -0600

    Probando el primer commit

---
+++ src/holagit/HolaGit.java
@@ -0,0 +1,16 @@
+
+package holagit;
+
+import javax.swing.JOptionPane;
+
+
+public class HolaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hola Mundo Git");
+    }
+    
+}
